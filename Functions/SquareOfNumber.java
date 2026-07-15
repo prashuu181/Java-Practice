b494f25ad2433f40d18cb159004d72af3c45ee01
RAYABATTU SURYA PRASHANTHI <prashuu181@gmail.com>
2026-07-15 22:10:59 +0530

    Add Square of Number function

---
+++ Functions/SquareOfNumber.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Prash{
+    static void PrashMethod(){
+        Scanner sc  = new Scanner(System.in);
+        System.out.println("Enter the value of n:");
+        int n = sc.nextInt();
+        int result = n*n;
+        System.out.print(result);
+    }
+    public static void main(String[] args){
+        PrashMethod();
+    }
+}
+
